335fb0fd07e4c5d8ae289883da47880981f6f6ea
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-24 15:07:59 +0300

    1D array that recieves inputs and returns the values

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main(){
     int student[5];
     int i;
+    cout<<"Enter the marks of the five students: \n";
     for(i=0; i<5; i++){
-        cin>>student[i];
+        
+        cin>> student[i];
     }
+    
+    cout<<"The output for the above code is: ";
+    for(i=0; i<5; i++){
+        cout<< "\t" <<student[i] <<"\n";
+    }
+    return 0;
 }
