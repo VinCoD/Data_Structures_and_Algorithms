bcfd452316f84ae61ae7a24247df0fe7200f1607
VinCoD <5895.2020@students.ku.ac.ke>
2022-05-03 15:36:18 +0300

    Created a multidimensional Array

---
+++ arrays/twodimensionmatrix.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, first[10][10], second[10][10], sum[10][10];
+    cout <<"Enter values of first matrix: \n";
+        for(i=0; i<2; i++){
+            for(j=0; j<3; j++){
+                cin>>first[i][j];
+                
+            }
+            cout<<"\n";
+        }
+        for(i=0; i<2; i++){
+            for(j=0; j<3; j++){
+                cout<<first[i][j];
+                cout<<"\t";
+                
+            }
+            cout<<"\n";
+        }
+        cout <<"Enter values of second matrix: \n";
+        for(i=0; i<2; i++){
+            for(j=0; j<3; j++){
+                cin>>second[i][j];
+                
+            }
+            cout<<"\n";
+        }
+        for(i=0; i<2; i++){
+            for(j=0; j<3; j++){
+                cout<<second[i][j];
+                cout<<"\t";
+                
+            }
+            cout<<"\n";
+        }
+        
+        for(i=0; i<2; i++){
+            for(j=0; j<3; j++){
+                sum[i][j] = first[i][j] + second[i][j];
+                
+            }
+            cout<<"\n";
+        }
+        
+        cout<<"The output for the Matrix is: \n";
+        for(i=0; i<2; i++){
+            for(j=0; j<3; j++){
+                cout<<sum[i][j];
+                cout<<"\t";
+                
+            }
+            cout<<"\n";
+        }
+    return 0;
+}
